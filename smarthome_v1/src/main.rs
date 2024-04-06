77b30e6d76186f160265000548bab97f931da38b
Aleksey Mayer <agm@interceptor.local>
2024-04-07 01:30:46 +0300

    Finish smarthome_v1 homework

--- smarthome_v1/src/main.rs
+++ smarthome_v1/src/main.rs
@@ -1,6 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+/*
+Умная розетка умеет:
+    Предоставлять текстовое описание.
+    Включаться и выключаться.
+    Предоставлять данные о текущей потребляемой мощности.
+Термометр умеет:
+    Выдавать данные о текущей температуре.
+*/
+
+struct SmartSocket {
+    description: String,
+    power_consumption: String,
+    status: String,
+}
+struct SmartThermometer {
+    current_temp: String,
+} 
+
+impl SmartSocket{
+    fn description(&self) {
+        println!("SmartSocket description {}", self.description)
+    }
+    fn power_consumption(&self) {
+        println!("SmartSocket power consumption is {}", self.power_consumption)
+    }
+    fn status(&self) {
+        println!("SmartSocket power is {}", self.status)
+    }
 }
 
-struct SmartSocket {}
-struct SmartTermometer {}
+impl SmartThermometer {
+    fn current_temp(&self) {
+        println!("Current temperature is {}", self.current_temp)
+    }
+}
+
+
+fn main() {
+    let socket = SmartSocket { description: String::from(""), power_consumption: String::from("1W"), status: String::from("on")};
+    let thermometer = SmartThermometer { current_temp: String::from("24 celcius")};
+
+    socket.description();
+    socket.power_consumption();
+    socket.status();
+
+    thermometer.current_temp();
+}
